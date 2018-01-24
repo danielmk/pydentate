66fa1a598515497235b77607ef1e51725520f6dc
Daniel Müller <danielmuellermsc@gmail.com>
2018-01-24 16:17:30 +0100

    Misc

--- tmgsyn_test.py
+++ tmgsyn_test.py
@@ -8,33 +8,44 @@ Created on Thu Jan 04 15:26:50 2018
 from neuron import h, gui
 import numpy as np
 import matplotlib.pyplot as plt
-from pyDentate.granulecell import GranuleCell
 from pyDentate.mossycell_cat import MossyCell
 
-h.nrn_load_dll("C:\\Users\\daniel\\repos\\nrnmech.dll")
+h.nrn_load_dll("C:\\Users\\DanielM\\Repos\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll")
 
-post = h.Section()
-
+"""Setup stimulation pattern"""
 t_pattern = np.arange(50,350,33)
-
-mc = MossyCell()
-
 vecstim = h.VecStim()
 pattern_vec = h.Vector(t_pattern)
 vecstim.play(pattern_vec)
 
-curr_syn = h.tmgsyn(mc.all_secs[1](0.5))
-#curr_syn.tau1 = tau1
-#curr_syn.tau2 = tau2
-curr_syn.e = 0
-curr_syn.tau_facil = 30 # This parameter gives the frequency dependence of facilitation
-curr_netcon = h.NetCon(vecstim, curr_syn)
-curr_netcon.weight[0] = 0.05
+"""Setup tmgsyn"""
+mc_tmgsyn = MossyCell()
 
-rec_g = h.Vector()
-rec_g.record(curr_syn._ref_g)
-rec_v = h.Vector()
-rec_v.record(mc.soma(0.5)._ref_v)
+mc_tmgsyn_syn = h.tmgsyn(mc_tmgsyn.all_secs[1](0.5))
+#curr_syn.tau1 = tau1
+#curr_syn.e = 0
+#curr_syn.tau_facil = 33 # This parameter gives the frequency dependence of facilitation
+#curr_syn.tau_1 = 1
+mc_tmgsyn_netcon = h.NetCon(vecstim, mc_tmgsyn_syn)
+mc_tmgsyn_netcon.weight[0] = 0.05
+
+mc_tmgsyn_rec_g = h.Vector()
+mc_tmgsyn_rec_g.record(mc_tmgsyn_syn._ref_g)
+mc_tmgsyn_rec_v = h.Vector()
+mc_tmgsyn_rec_v.record(mc_tmgsyn.soma(0.5)._ref_v)
+
+"""Setup Exp2Syn"""
+mc_exp2syn = MossyCell()
+
+mc_exp2syn_syn = h.Exp2Syn(mc_exp2syn.all_secs[1](0.5))
+mc_exp2syn_netcon = h.NetCon(vecstim, mc_exp2syn_syn)
+mc_exp2syn_netcon.weight[0] = 0.05
+
+mc_exp2syn_rec_g = h.Vector()
+mc_exp2syn_rec_g.record(mc_exp2syn_syn._ref_g)
+mc_exp2syn_rec_v = h.Vector()
+mc_exp2syn_rec_v.record(mc_exp2syn.soma(0.5)._ref_v)
+
 
 dt = 0.1
 h.steps_per_ms = 1.0/dt
