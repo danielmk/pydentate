f39bb5738ab12aeff2ec723629cd7be97a429b82
Daniel Müller <danielmuellermsc@gmail.com>
2018-01-29 10:19:01 +0100

    Misc

--- frequencystim_hilus_paradigm.py
+++ frequencystim_hilus_paradigm.py
@@ -7,7 +7,6 @@ Auto init and run
 """
 
 from neuron import h
-import ouropy
 import numpy as np
 import standard_net
 import time
@@ -15,36 +14,35 @@ import time
 h.nrn_load_dll("C:\\Users\\DanielM\\Repos\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll")
 
 # Setup specs of the paradigm
-n_cells = 800 # Number of cells that are stimulated
+n_cells = 800  # Number of cells that are stimulated
 stim_amp = 1
 stim_dur = 6
 stim_delay = 100
 idx_measured_mc = 12
 stim_periods = [1000, 100, 33, 20] # in ms
-reps = 1 # Number of repetitions for the networks
+#stim_periods = [1000]
+reps = 1  # Number of repetitions for the networks
 
 for period in stim_periods:
-    
+    stim_times = np.arange(100, 100+10*period, period)
     for rep in range(reps):
         # Create a standard networks and add the stimulation
         ctrl_nw = standard_net.StandardNetwork(seed = 10000 + rep)
-        target_pool = range(trial, trial + 200)
-        targets = np.random.choice(target_pool, n_cells, replace = False)
-        targets = np.delete(targets,np.argwhere(targets == idx_measured_cell))
-        ctrl_nw.populations[0].current_clamp_range(targets,
-                                                   amp = stim_amp,
-                                                   dur = stim_dur,
-                                                   delay = stim_delay)
-        ctrl_vclamp_cell = ctrl_nw.populations[0].cells[500]
-        ctrl_vclamp_cell._vclamp()
+        #targets = np.random.choice(2000, n_cells, replace = False)
+        ctrl_nw.populations[0].mk_current_clamp(n_cells,
+                                               amp = stim_amp,
+                                               dur = stim_dur,
+                                               delays = stim_times)
+        ctrl_vclamp_cell = ctrl_nw.populations[1].cells[12]
+        ctrl_vclamp_cell._vclamp(dur1=200, amp1=-70, rs=0.001)
         ctrl_i_vec = h.Vector()
         ctrl_i_vec.record(ctrl_vclamp_cell.vclamp._ref_i)
         ctrl_volt_rec = ctrl_vclamp_cell._voltage_recording()
-        
+
         """Initialization for -2000 to -100"""
-        #print("Running trial " + str(trial))
+        #print("Running rep " + str(rep))
         h.cvode.active(0)
-        dt = 0.01
+        dt = 0.1
         h.steps_per_ms = 1.0/dt
         h.tstop = 1500
         h.finitialize(-60)
@@ -55,21 +53,21 @@ for period in stim_periods:
             h.fadvance()
         h.secondorder = 2
         h.t = 0
-        h.dt = 0.01
+        h.dt = 0.1
         
         """Setup run control for -100 to 1500"""
         h.frecord_init()  # Necessary after changing t to restart the vectors
-        while h.t < 200:
+        while h.t < stim_times[9]+500:
             h.fadvance()
 
-        print("Saving trial " + str(trial))
-        nw.populations[0].write_aps("GranuleCells_trial_" + str(trial) + "stim_pos_" + str(stim_pos))
-        nw.populations[1].write_aps("MossyCells_" + str(trial) + "stim_pos_" + str(stim_pos))
-        nw.populations[2].write_aps("BasketCells_" + str(trial) + "stim_pos_" + str(stim_pos))
-        nw.populations[3].write_aps("HIPPCells_" + str(trial) + "stim_pos_" + str(stim_pos))
+        print("Saving rep " + str(rep))
+        ctrl_nw.populations[0].write_aps("GranuleCells_period_" + str(period) + "_rep_" + str(rep))
+        ctrl_nw.populations[1].write_aps("MossyCells_period_" + str(period) + "_rep_" + str(rep))
+        ctrl_nw.populations[2].write_aps("BasketCells_period_" + str(period) + "_rep_" + str(rep))
+        ctrl_nw.populations[3].write_aps("HIPPCells_period_" + str(period) + "_rep_" + str(rep))
         
-        f_name = "volt_clamp_trial_" + str(trial) + "stim_pos_" + str(stim_pos)
+        f_name = "volt_clamp_period_" + str(period) + "_rep_" + str(rep)
         
-        np.savez(f_name, nw.vclamp_vec.as_numpy())
+        np.savez(f_name, ctrl_i_vec.as_numpy())
         
         stop_save = time.clock()
--- mfstim_model.py
+++ mfstim_model.py
@@ -17,17 +17,17 @@ from hippcell import HippCell
 h.nrn_load_dll("C:\\Users\\Holger\\danielm\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll")
 
 class MFStimNetwork(ouropy.gennetwork.GenNetwork):
-    
+
     def __init__(self, seed = None, n_cells = 200, stim_int = 20):
         #Setup cells
         self.mk_population(GranuleCell, 500)
         self.mk_population(MossyCell, 15) 
         self.mk_population(BasketCell, 6)
         self.mk_population(HippCell, 6)
-        
+
         #Set seed for reproducibility
         self.set_numpy_seed(seed)
-        
+
         # Setup recordings
         self.populations[0].record_aps()
         self.populations[1].record_aps()
@@ -37,12 +37,11 @@ class MFStimNetwork(ouropy.gennetwork.GenNetwork):
         # Set up artificial mf stimulation
         self.iclamped_cells = self.populations[0].current_clamp_rnd(n_cells, amp=1, dur=6, delay=np.arange(100,100+10*stim_int,stim_int))
 
-
         self.vclamp_vec = h.Vector()
         vclamp_cell = np.random.choice(self.populations[0].cells, 1, replace = False)[0]
         while vclamp_cell in self.iclamped_cells:
             vclamp_cell = np.random.choice(self.populations[0].cells, 1, replace = False)[0]
-        
+
         vclamp_cell._vclamp()
         self.vclamp_vec.record(vclamp_cell.vclamp._ref_i)
         self.volt_record = vclamp_cell._voltage_recording()
@@ -52,35 +51,35 @@ class MFStimNetwork(ouropy.gennetwork.GenNetwork):
         (self, pre_pop, post_pop, target_pool,
          target_segs, divergence, tau1, tau2, e, thr, delay, weight)
         """
-        
+
         # GC -> MC
         self.mk_Exp2SynConnection(self.populations[0], self.populations[1], 3,
                                    'proxd', 1, 0.5,6.2, 0, 10, 1.5, 0.2*10**(-3))
-        
+
         # GC -> BC
         self.mk_Exp2SynConnection(self.populations[0], self.populations[2], 3,
                                    'proxd', 1, 0.3, 0.6, 0, 10, 0.8, 4.7*10**(-3))
-        
+
         # GC -> HC
         self.mk_Exp2SynConnection(self.populations[0], self.populations[3], 5,
                                    'proxd', 3, 0.3, 0.6, 0, 10, 1.5, 0.5*10**(-3))
-        
+
         # MC -> GC
         """self.mk_Exp2SynConnection(self.populations[1], self.populations[0], 350,
                                    'proxd', 200, 1.5, 5.5, 0, 10, 3, 0.3*10**(-3))"""
-        
+
         # MC -> MC
         self.mk_Exp2SynConnection(self.populations[1], self.populations[1], 6,
                                    'proxd', 1, 0.45, 2.2, 0, 10, 2, 0.5*10**(-3))
-        
+
         # MC -> BC
         self.mk_Exp2SynConnection(self.populations[1], self.populations[2], 3,
                                    'proxd', 1, 0.1, 0.1, 1, 10, 3, 0.3*10**(-3))
-        
+
         # MC -> HC
         self.mk_Exp2SynConnection(self.populations[1], self.populations[3], 5,
                                    'midd', 2, 0.9, 3.6, 0, 10, 3,0.2*10**(-3))
-        
+
         # BC -> GC
         #ORIGINAL
         """self.mk_Exp2SynConnection(self.populations[2], self.populations[0], 140,
@@ -92,22 +91,22 @@ class MFStimNetwork(ouropy.gennetwork.GenNetwork):
         # BC -> MC
         self.mk_Exp2SynConnection(self.populations[2], self.populations[1], 7,
                                    'proxd', 3, 0.3, 3.3, -70, -10, 1.5, 1.5*10**(-3))
-        
+
         # BC -> BC
         self.mk_Exp2SynConnection(self.populations[2], self.populations[2], 3,
                                    'proxd', 2, 0.16, 1.8, -70, -10, 0.8, 7.6*10**(-3))
-        
+
         # HC -> GC
         #ORIGINAL
         """self.mk_Exp2SynConnection(self.populations[3], self.populations[0], 260,
                                    'dd', 160, 0.5, 6, -70, 10, 1.6, 0.5*10**(-3))"""
         self.mk_Exp2SynConnection(self.populations[3], self.populations[0], 260,
                                    'dd', 160, 1, 18, -70, 10, 1.6, 0.5*10**(-3))
-        
+
         # HC -> MC
         self.mk_Exp2SynConnection(self.populations[3], self.populations[1], 5,
                                    ['mid1d', 'mid2d'], 4, 0.5, 6, -70, 10, 1, 1.5*10**(-3))
-        
+
         # HC -> BCa
         self.mk_Exp2SynConnection(self.populations[3], self.populations[2], 5,
                                    'ddend', 4, 0.4, 5.8, -70, 10, 1.6, 0.5*10**(-3))
