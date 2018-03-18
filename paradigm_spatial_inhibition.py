8827f786c3db6bc05370c9dfce65bf146c24832f
danielmuellermsc <danielmuellermsc@gmail.com>
2018-03-18 22:44:25 +0100

    Misc

--- paradigm_spatial_inhibition.py
+++ paradigm_spatial_inhibition.py
@@ -7,39 +7,39 @@ Auto init and run
 """
 
 from neuron import h
-import ouropy
 import numpy as np
 import net_tuned
-import time
+import matplotlib.pyplot as plt
 
 # Office PC
-h.nrn_load_dll("C:\\Users\\DanielM\\Repos\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll")
-#Home PC
-#h.nrn_load_dll("C:\\Users\\daniel\\repos\\nrnmech.dll")
+# h.nrn_load_dll("C:\\Users\\DanielM\\Repos\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll")
+# Home PC
+h.nrn_load_dll("C:\\Users\\daniel\\repos\\nrnmech.dll")
 
 # Setup specs for stimulation
-n_cells = 100 # Number of cells that are stimulated
-stim_pool = 150 # Size of the pool from which stimulated cells are chosen
+n_cells = 100  # Number of cells that are stimulated
+stim_pool = 150  # Size of the pool from which stimulated cells are chosen
 stim_location = int(2000 / 2.0 - stim_pool / 2.0)
-stim_amp = 1
-stim_dur = 6
-stim_delay = 100
+stim_amp = 1.5
+stim_dur = 15
+stim_delay = 50
 
 # Setup specs for measurements
-cells_to_measure = range(0,2000, 50)
+cells_to_measure = range(0, 2000, 50)
 
-save_dir = "C:\\Users\\daniel\\repos\\pyDentate\\paradigm_spatial_inhibition_saves_2018-03-11"
+save_dir = "C:\\Users\\daniel\\repos\\pyDentate\\paradigm_spatial_inhibition_saves_2018-03-18"
 
-for run in range(1):
+for run in range(2,10):
     # Create a standard networks and add the stimulation
-    nw = net_tuned.TunedNetwork(seed = 10000 + run)
-    stim_cells = np.random.choice(range(stim_location,stim_location+stim_pool), n_cells, replace = False)
+    nw = net_tuned.TunedNetwork(seed=10000+run)
+    np.random.seed(10000 + run)
+    stim_cells = np.random.choice(range(stim_location, stim_location+stim_pool), n_cells, replace = False)
     nw.populations[0].current_clamp_range(stim_cells,
-                                               amp = stim_amp,
-                                               dur = stim_dur,
-                                               delay = stim_delay)
+                                          amp=stim_amp,
+                                          dur=stim_dur,
+                                          delay=stim_delay)
     
-    nw.populations[0].SEClamp(cells_to_measure)
+    nw.populations[0].SEClamp(cells_to_measure, dur1 = 100, rs=1)
     nw.populations[0].voltage_recording(stim_cells)
 
     """Initialization for -2000 to -100"""
@@ -57,17 +57,38 @@ for run in range(1):
     h.secondorder = 2
     h.t = 0
     h.dt = 0.01
-    
+
     """Setup run control for -100 to 1500"""
     h.frecord_init()  # Necessary after changing t to restart the vectors
-    while h.t < 200:
+    while h.t < 100:
         h.fadvance()
 
-    """nw.populations[0].write_aps("GranuleCells_trial_" + str(trial) + "stim_pos_" + str(stim_pos))
-    nw.populations[1].write_aps("MossyCells_" + str(trial) + "stim_pos_" + str(stim_pos))
-    nw.populations[2].write_aps("BasketCells_" + str(trial) + "stim_pos_" + str(stim_pos))
-    nw.populations[3].write_aps("HIPPCells_" + str(trial) + "stim_pos_" + str(stim_pos))
+    spike_plot = nw.plot_aps()
+    spike_plot_file_name = "run_" + str(run) + "_spike_plot"
+    nw.save_ap_fig(spike_plot, directory = save_dir, file_name = spike_plot_file_name)
+    data_file_name = "run_" + str(run) + "_data"
+    nw.shelve_network(directory = save_dir, file_name = data_file_name)
     
-    f_name = "volt_clamp_trial_" + str(trial) + "stim_pos_" + str(stim_pos)
+    # Calculate spatial IPSC plot
+    sampling_period = h.dt
+    bl_times = np.array([40, 50])  # in ms
+    IPSC_times = np.array([50, 90])  # in ms
+    bl_dtps = bl_times / sampling_period
+    IPSC_dtps = IPSC_times / sampling_period
     
-    np.savez(f_name, nw.vclamp_vec.as_numpy())"""
+    IPSCs = []
+    for cell_i in nw.populations[0].VClamps_i:
+        trace = cell_i.as_numpy()
+        bl = trace[int(bl_dtps[0]):int(bl_dtps[1])].mean()
+        peak_IPSC = trace[int(IPSC_dtps[0]):int(IPSC_dtps[1])].max()
+        IPSCs.append(peak_IPSC - bl)
+    spatial_plot = plt.figure()
+    plt.plot(cells_to_measure, IPSCs)
+    plt.xlabel("Granule Cell #")
+    plt.ylabel("Peak IPSC (nA)")
+    full_file_path = save_dir + '\\' + 'run_' + str(run) + '_spatial_IPSC_plot'
+    spatial_plot.savefig(full_file_path + ".pdf", dpi = 300, format ='pdf')
+    spatial_plot.savefig(full_file_path + ".eps", dpi = 300, format ='eps')
+
+    
+
