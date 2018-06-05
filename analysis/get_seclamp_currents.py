ef33db79715cb3043123b83f66a187c2207a9b6b
Daniel Müller <danielmuellermsc@gmail.com>
2018-06-05 20:14:05 +0200

    Misc

--- analysis/get_seclamp_currents.py
+++ analysis/get_seclamp_currents.py
@@ -7,6 +7,25 @@ Created on Sun May 20 11:35:03 2018
 
 import os
 import numpy as np
+import shelve
 
-data_path = "C:\\Users\\DanielM\\Repos\\pyDentateData\\frequency_inhibition_data"
-onlyfiles = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
+# Setup some parameters given by paradigm_frequency_inhibition.py
+stim_delay = 100  # ms
+dt = 0.01  # ms
+stim_dtp = stim_delay / dt
+
+data_path = "C:\\Users\\DanielM\\Repos\\pyDentateData\\frequency_inhibition_data\\"
+save_path = "C:\\Users\\DanielM\\Repos\\pyDentateData\\frequency_inhibition_data\\"
+data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.pydd' in f]
+
+for x in data_files:
+    interval = int(x.split('_')[8].split('.')[0][1:3])
+    data = shelve.open(data_path + x)
+    split_name_current = x.split('.')
+    split_name_peaks = list(split_name_current)
+    split_name_current[1] = split_name_current[1] + '_current'
+    split_name_peaks[1] = split_name_peaks[1] + '_peaks'
+    name_current = '.'.join(split_name_current)
+    name_peaks = '.'.join(split_name_peaks)
+    np.savez(save_path + name_current, np.array(data[data.keys()[0]]['populations'][0]['VClamps_i']))
+        
---
+++ analysis/plot_frequency_inhibition.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 20 11:35:03 2018
+
+@author: DanielM
+"""
+
+import os
+import numpy as np
+import shelve
+import matplotlib.pyplot as plt
+
+# Setup some parameters given by paradigm_frequency_inhibition.py
+stim_delay = 100  # ms
+dt = 0.01  # ms
+stim_dtp = stim_delay / dt
+
+data_path = "C:\\Users\\DanielM\\Repos\\pyDentateData\\frequency_inhibition_data\\"
+save_path = "C:\\Users\\DanielM\\Repos\\pyDentateData\\frequency_inhibition_data\\"
+data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.npz' in f and '40' in f and '20' in f and not '33' in f]
+
+data = np.load(data_path + data_files[0])['arr_0']
+for x in data_files[0:len(data_files)-1]:
+    data = np.concatenate((data,np.load(data_path + x)['arr_0']))
+    #data = np.load(data_path + x)['arr_0']
+    #plt.plot(np.arange(0,500.02, 0.01), data.mean(axis=0))
+    #for y in data:
+    #    plt.plot(np.arange(0,500.02, 0.01), y)
