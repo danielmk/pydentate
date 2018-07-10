2dd44f1e913f37dfab5400671656b4290c8f5d99
unknown <danielmuellermsc@gmail.com>
2018-07-10 23:52:56 +0200

    Misc

--- rd/local_vs_global_stochastics.py
+++ rd/local_vs_global_stochastics.py
@@ -12,10 +12,31 @@ np.random.seed(10000)
 n_cells = 24
 local_pool = 560
 global_pool = 2000
+n_synapses = 400
 
+bin_size = 1
+
+start_local = (global_pool - local_pool) / 2.0
 # Draw local targets
 local_targets = []
 for x in range(n_cells):
-    local_targets.append(np.random.choice(np.arange(local_pool), 400, replace=False))
-    
+    local_targets.append(np.random.choice(np.arange(start_local, start_local + local_pool), n_synapses, replace=False))
+
 local_targets = np.array(local_targets)
+plt.figure()
+local_hist = plt.hist(local_targets.flatten(), bins = np.arange(-0.5,global_pool, bin_size))
+plt.xlabel("# GC")
+plt.ylabel("# Synapses")
+plt.title("24 locally connecting cells")
+
+# Draw global targets
+global_targets = []
+for x in range(n_cells):
+    global_targets.append(np.random.choice(np.arange(0, global_pool), n_synapses, replace=False))
+
+global_targets = np.array(global_targets)
+plt.figure()
+global_hist = plt.hist(global_targets.flatten(), bins = np.arange(-0.5,global_pool, bin_size))
+plt.xlabel("# GC")
+plt.ylabel("# Synapses")
+plt.title("24 globally connecting cells")
