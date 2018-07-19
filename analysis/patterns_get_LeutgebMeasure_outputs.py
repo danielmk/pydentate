283a35e8e271069102eb7a154bdbe50f06a75f26
unknown <danielmuellermsc@gmail.com>
2018-07-19 15:40:19 +0200

    Added __name__ __main__ construct to similarity measure scripts

--- analysis/patterns_get_LeutgebMeasure_outputs.py
+++ analysis/patterns_get_LeutgebMeasure_outputs.py
@@ -11,30 +11,31 @@ import shelve
 import os
 import analysis_main
 
-# Setup some parameters given by paradigm_frequency_inhibition.py
-data_path = "C:\\Users\\Daniel\\pyDentateData\\pattern_separation_data_local_repeated_input_revised\\seed10000\\scale1000\\net_nonfacilitatingrev\\"
-save_path = data_path
-data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.npz' in f and 'spike_data' in f and not 'convolved' in f]
-data_files.sort()
-
-data_files = data_files[0:25]
-
-corr_matrix = np.empty((len(data_files), len(data_files)))
-
-data_list = []
-for x in data_files:
-    data_list.append(np.load(data_path + x)['arr_0'])
-
-row_idx_start = 0
-row_idx_stop = 25
-# 376
-len_bins = 6000
-
-for row_idx, x in enumerate(data_list[row_idx_start:row_idx_stop]):
-    for col_idx, y in enumerate(data_list[row_idx+row_idx_start:len(data_list)]):
-        corr_matrix[row_idx+row_idx_start,col_idx+row_idx+row_idx_start]=analysis_main.similarity_measure_leutgeb(x,y,len_bins)
-        
-np.savetxt(save_path + "1_leutgeb-measure_matrix_len-bin_" + str(len_bins) +  ".txt", corr_matrix, delimiter="\t")
+def similarity_measure_leutgeb_outputs_directory(data_path, len_bins):
+    """Calculate the leutgeb measures for all unique comparisons of output
+    patterns in the path directory"""
+    save_path = data_path
+    data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.npz' in f and 'spike_data' in f and not 'convolved' in f]
+    data_files.sort()
+    
+    data_files = data_files[0:25]
+    
+    corr_matrix = np.empty((len(data_files), len(data_files)))
+    
+    data_list = []
+    for x in data_files:
+        data_list.append(np.load(data_path + x)['arr_0'])
+    
+    row_idx_start = 0
+    row_idx_stop = 25
+    
+    for row_idx, x in enumerate(data_list[row_idx_start:row_idx_stop]):
+        for col_idx, y in enumerate(data_list[row_idx+row_idx_start:len(data_list)]):
+            corr_matrix[row_idx+row_idx_start,col_idx+row_idx+row_idx_start]=analysis_main.similarity_measure_leutgeb(x,y,len_bins)
+            
+    np.savetxt(save_path + "1_leutgeb-measure_matrix_len-bin_" + str(len_bins) +  ".txt", corr_matrix, delimiter="\t")
 
 if __name__ == '__main__':
-    
+    path = "C:\\Users\\Daniel\\pyDentateData\\pattern_separation_data_local_input_revised\\seed10000\\scale1000\\net_reshuffledrev\\"
+    len_bins = 6000
+    similarity_measure_leutgeb_outputs_directory(path, len_bins)
--- analysis/patterns_get_LeutgebMeasure_tresolved_inputs.py
+++ analysis/patterns_get_LeutgebMeasure_tresolved_inputs.py
@@ -11,29 +11,33 @@ import shelve
 import os
 import analysis_main
 
-# Setup some parameters given by paradigm_frequency_inhibition.py
-data_path = "C:\\Users\\Daniel\\pyDentateData\\pattern_separation_data_local_input_revised\\seed10000\\input_patterns_seed_10000\\"
-save_path = data_path
-data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.npz' in f and not 'norm' in f and not 'trifilt' in f]
-data_files.sort()
 
-data_files = data_files[0:25]
-
-corr_matrix = np.empty((len(data_files), len(data_files)))
-
-data_list = []
-for x in data_files:
-    data_list.append(np.load(data_path + x)['arr_0'])
-
-row_idx_start = 0
-row_idx_stop = 25
-# 376
-len_bin = 1000
-
-outputs = []
-for row_idx, x in enumerate(data_list[row_idx_start:row_idx_stop]):
-    for col_idx, y in enumerate(data_list[row_idx+row_idx_start:len(data_list)]):
-        outputs.append(analysis_main.similarity_measure_leutgeb(x,y,len_bin))
-        
-outputs = np.array(outputs)
-np.savetxt(save_path + "1_leutgeb-measure_matrix_tresolved_len-bin_" + str(len_bin) + ".txt", outputs, delimiter="\t")
+def similarity_measure_leutgeb_inputs_directory(data_path, len_bins):
+    # Setup some parameters given by paradigm_frequency_inhibition.py
+    save_path = data_path
+    data_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and '.npz' in f and not 'norm' in f and not 'trifilt' in f]
+    data_files.sort()
+    
+    data_files = data_files[0:25]
+    
+    corr_matrix = np.empty((len(data_files), len(data_files)))
+    
+    data_list = []
+    for x in data_files:
+        data_list.append(np.load(data_path + x)['arr_0'])
+    
+    row_idx_start = 0
+    row_idx_stop = 25
+    
+    outputs = []
+    for row_idx, x in enumerate(data_list[row_idx_start:row_idx_stop]):
+        for col_idx, y in enumerate(data_list[row_idx+row_idx_start:len(data_list)]):
+            outputs.append(analysis_main.similarity_measure_leutgeb(x,y,len_bins))
+            
+    outputs = np.array(outputs)
+    np.savetxt(save_path + "1_leutgeb-measure-tresolved_len-bin_" + str(len_bins) + ".txt", outputs, delimiter="\t")
+    
+if __name__ == '__main__':
+    path = "C:\\Users\\Daniel\\pyDentateData\\pattern_separation_data_local_input_revised\\seed10000\\input_patterns_seed_10000\\"
+    len_bins = 1000
+    similarity_measure_leutgeb_inputs_directory(path, len_bins)
