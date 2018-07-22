505483de39850d19af093f6c2cc1703c694695f2
unknown <danielmuellermsc@gmail.com>
2018-07-22 23:55:17 +0200

    Misc

--- paradigm_local_pattern_separation.py
+++ paradigm_local_pattern_separation.py
@@ -7,63 +7,72 @@ Created on Mon Mar 05 13:41:23 2018
 
 from neuron import h
 import numpy as np
-import net_tunedrevexp
+import net_globalrevexp
 from burst_generator_inhomogeneous_poisson import inhom_poiss
 import os
 import argparse
 import scipy.stats as stats
 
-# Handle command line inputs with argparse
-parser = argparse.ArgumentParser(description='Local pattern separation paradigm')
-parser.add_argument('-runs',
+# Parse command line inputs
+# Command line signature:
+# python script -runs n n n -savedir str -scale n -seed n
+pr = argparse.ArgumentParser(description='Local pattern separation paradigm')
+pr.add_argument('-runs',
                     nargs=3,
                     type=int,
                     help='start stop range for the range of runs',
                     default=[0, 1, 1],
                     dest='runs')
-parser.add_argument('-savedir',
+pr.add_argument('-savedir',
                     type=str,
                     help='complete directory where data is saved',
                     default=os.getcwd(),
                     dest='savedir')
-parser.add_argument('-scale',
+pr.add_argument('-scale',
                     type=int,
                     help='standard deviation of gaussian distribution',
                     default=500,
                     dest='input_scale')
-parser.add_argument('-seed',
+pr.add_argument('-seed',
                     type=int,
                     help='standard deviation of gaussian distribution',
                     default=10000,
                     dest='seed')
 
-args = parser.parse_args()
+args = pr.parse_args()
 runs = range(args.runs[0], args.runs[1], args.runs[2])
 savedir = args.savedir
 input_scale = args.input_scale
 seed = args.seed
 
-# Locate a nrnmech.dll file that has the mechanisms required by the network
-# On your own machine you have to add the path to your own file to the list dll_files
-dll_files = ["C:\\Users\\DanielM\\Repos\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll",
-            "C:\\Users\\daniel\\Repos\\nrnmech.dll",
-            "C:\\Users\\Holger\\danielm\\models_dentate\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll",
-            "C:\\Users\\Daniel\\repos\\dentate_gyrus_Santhakumar2005_and_Yim_patterns\\dentategyrusnet2005\\nrnmech.dll"]
+# Locate a nrnmech.dll file containig the mechanisms required by the network
+# adapt path for your own machine.
+dll_files = [("C:\\Users\\DanielM\\Repos\\models_dentate\\"
+              "dentate_gyrus_Santhakumar2005_and_Yim_patterns\\"
+              "dentategyrusnet2005\\nrnmech.dll"),
+              "C:\\Users\\daniel\\Repos\\nrnmech.dll",
+             ("C:\\Users\\Holger\\danielm\\models_dentate\\"
+              "dentate_gyrus_Santhakumar2005_and_Yim_patterns\\"
+              "dentategyrusnet2005\\nrnmech.dll"),
+             ("C:\\Users\\Daniel\\repos\\"
+              "dentate_gyrus_Santhakumar2005_and_Yim_patterns\\"
+              "dentategyrusnet2005\\nrnmech.dll")]
 for x in dll_files:
     if os.path.isfile(x):
         dll_dir = x
-print("DLL loaded from: " + str(dll_dir))
+print("DLL loaded from: " + dll_dir)
 h.nrn_load_dll(dll_dir)
 
+# Seed the numpy random number generator for replication
 np.random.seed(seed)
-# Generate a gaussian probability density function
+
+# Randomly choose target cells for the PP lines
 gauss_gc = stats.norm(loc=1000, scale=input_scale)
 gauss_bc = stats.norm(loc=12, scale=(input_scale/2000.0)*24)
 pdf_gc = gauss_gc.pdf(np.arange(2000))
 pdf_gc = pdf_gc/pdf_gc.sum()
 pdf_bc = gauss_bc.pdf(np.arange(24))
 pdf_bc = pdf_bc/pdf_bc.sum()
-# We hold the pdf constant. To randomize the centroid we reslice the GC indices
 GC_indices = np.arange(2000)
 start_idc = np.random.randint(0, 1999, size=400)
 
@@ -73,7 +82,7 @@ for x in start_idc:
     PP_to_GCs.append(np.random.choice(curr_idc, size=100, replace=False, p=pdf_gc))
 
 PP_to_GCs = np.array(PP_to_GCs)
-# Generate the PP -> BC mapping as above
+
 BC_indices = np.arange(24)
 start_idc = np.array(((start_idc/2000.0)*24), dtype=int)
 
@@ -90,7 +99,7 @@ temporal_patterns = inhom_poiss()
 
 # Start the runs of the model
 for run in runs:
-    nw = net_tunedrevexp.TunedNetwork(seed, temporal_patterns[0+run:24+run],
+    nw = net_globalrevexp.TunedNetwork(seed, temporal_patterns[0+run:24+run],
                                 PP_to_GCs[0+run:24+run],
                                 PP_to_BCs[0+run:24+run])
 
