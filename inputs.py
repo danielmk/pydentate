fbdaa9d79b97903a596179931ae158eea3011089
Barış Can Kuru <50696832+barisckuru@users.noreply.github.com>
2021-03-02 00:19:39 +0100

    Update inputs.py
    Grid cell model added (to be modified more)

--- inputs.py
+++ inputs.py
@@ -65,5 +65,77 @@ def _grid_field(spacing):
 def _grid_spatial_navigation():
     pass
 
+#Solstad 2006 Grid Model
+def _grid_maker(spacing, orientation, pos_peak, arr_size, sizexy, max_rate):
+    #define the params from input here, scale the resulting array for maxrate and sperate the xy for size and shift
+    arr_size = arr_size
+    x, y = pos_peak
+    pos_peak = np.array([x,y])
+    max_rate = max_rate
+    lambda_spacing = spacing*(arr_size/100) #100 required for conversion, they have probably used 100*100 matrix in 
+    k = (4*np.pi)/(lambda_spacing*np.sqrt(3))
+    degrees = orientation
+    theta = np.pi*(degrees/180)
+    meterx, metery = sizexy
+    arrx = meterx*arr_size # *arr_size for defining the 2d array size
+    arry = metery*arr_size
+    dims = np.array([arrx,arry])
+    rate = np.ones(dims)
+    #implementation of grid function
+    # 3 ks for 3 cos gratings with different angles
+    k1 = ((k/np.sqrt(2))*np.array((np.cos(theta+(np.pi)/12) + np.sin(theta+(np.pi)/12),
+          np.cos(theta+(np.pi)/12) - np.sin(theta+(np.pi)/12)))).reshape(2,)
+    k2 = ((k/np.sqrt(2))*np.array((np.cos(theta+(5*np.pi)/12) + np.sin(theta+(5*np.pi)/12),
+          np.cos(theta+(5*np.pi)/12) - np.sin(theta+(5*np.pi)/12)))).reshape(2,)
+    k3 = ((k/np.sqrt(2))*np.array((np.cos(theta+(9*np.pi)/12) + np.sin(theta+(9*np.pi)/12),
+          np.cos(theta+(9*np.pi)/12) - np.sin(theta+(9*np.pi)/12)))).reshape(2,)
+    
+    rate[i,j] = (np.cos(np.dot(k1, curr_dist))+
+               np.cos(np.dot(k2, curr_dist))+ np.cos(np.dot(k3, curr_dist)))/3
+    rate = max_rate*2/3*(rate+1/2)   # arr is the resulting 2d grid out of 3 gratings
+    return rate
+    
+def _grid_population(n_grid, max_rate, seed, arr_size=200):
+    # skewed normal distribution for grid_spc
+    np.random.seed(seed)
+    median_spc = 43
+    spc_max = 100
+    skewness = 6  #Negative values are left skewed, positive values are right skewed.
+    grid_spc = skewnorm.rvs(a = skewness,loc=spc_max, size=n_grid)  #Skewnorm function
+    grid_spc = grid_spc - min(grid_spc)      #Shift the set so the minimum value is equal to zero.
+    grid_spc = grid_spc / max(grid_spc)      #Standadize all the vlues between 0 and 1. 
+    grid_spc = grid_spc * spc_max         #Multiply the standardized values by the maximum value.
+    grid_spc = grid_spc + (median_spc - np.median(grid_spc))
+    
+    grid_ori = np.random.randint(0, high=60, size=[n_grid,1]) #uniform dist btw 0-60 degrees
+    grid_phase = np.random.randint(0, high=(arr_size-1), size=[n_grid,2]) #uniform dist grid phase
+    
+    # create a 3d array with grids for n_grid
+    rate_grids = np.zeros((arr_size, arr_size, n_grid))#empty array
+    for i in range(n_grid):
+        x = grid_phase[i][0]
+        y = grid_phase[i][1]
+        rate = grid_maker(grid_spc[i], grid_ori[i], [x, y], arr_size, [1,1], max_rate)
+        rate_grids[:, :, i] = rate
 
+    
+    return rate_grids, grid_spc
+
+
+def _inhom_poiss(arr, dur_s, poiss_seed=0, dt_s=0.025):
+    np.random.seed(poiss_seed)
+    n_cells = arr.shape[0]
+    spi_arr = np.zeros((n_cells), dtype = np.ndarray)
+    for grid_idc in range(n_cells):
+        np.random.seed(poiss_seed+grid_idc)
+        rate_profile = arr[grid_idc,:]
+        asig = AnalogSignal(rate_profile,
+                                units=1*pq.Hz,
+                                t_start=0*pq.s,
+                                t_stop=dur_s*pq.s,
+                                sampling_period=dt_s*pq.s,
+                                sampling_interval=dt_s*pq.s)
+        curr_train = stg.inhomogeneous_poisson_process(asig)
+        spi_arr[grid_idc] = np.array(curr_train.times*1000) #time conv to ms
+    return spi_arr
 
