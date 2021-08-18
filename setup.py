742d61ce4c4e4b60256c50076c5f158b72c5913a
danielmk <danielmuellermsc@gmail.com>
2021-08-18 13:04:36 +0200

    Add setup.py and environment.yml

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyDentate',
+    version='0.1.1',
+    description='pyDentate is a biophysically realistic computational model of the dentate gyrus.',
+    long_description=readme,
+    author='Daniel Müller-Komorowska',
+    author_email='danielmuellermsc@gmail.com',
+    url='https://github.com/danielmk/pyDentate',
+    license=license,
+    packages=['ouropy'],
+    install_requires=[
+          'elephant',
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'scipy',
+          'neo',
+          'neuron',
+      ])
