42d06e2840eff9e2f507cb305d3e2f2c56a4f8ef
barisckuru <barisckuru@gmail.com>
2021-10-19 15:26:22 +0000

    network tuning options were added to net_tunedrev

--- pydentate/net_tunedrev.py
+++ pydentate/net_tunedrev.py
@@ -36,6 +36,7 @@ class TunedNetwork(gennetwork.GenNetwork):
     def __init__(self, seed=None, temporal_patterns=np.array([]),
                  spatial_patterns_gcs=np.array([]),
                  spatial_patterns_bcs=np.array([]),
+                 network_type='full',
                  pp_weight=1e-3):
         self.init_params = locals()
         self.init_params['self'] = str(self.init_params['self'])
@@ -55,7 +56,36 @@ class TunedNetwork(gennetwork.GenNetwork):
         self.populations[2].record_aps()
         self.populations[3].record_aps()
 
-        temporal_patterns = np.array(temporal_patterns)
+        temporal_patterns = np.array(temporal_patterns, dtype=object)
+
+        # weights
+        # feedforward inhibition
+        pp_bc = pp_weight
+        # feedback inhibition
+        gc_bc = 2.5e-2
+        gc_hc = 2.5e-2
+        gc_mc = 2e-2
+        # complete inhibition
+        bc_gc = 1.2e-3
+        hc_gc = 6e-3
+
+        if network_type == "no-feedback":
+            # Set GC to BC, HC and MC weights to 0
+            gc_bc, gc_hc, gc_mc = 0, 0, 0
+
+        elif network_type == "no-feedforward":
+            # Set PP to BC weight to 0
+            pp_bc = 0
+
+        elif network_type == "disinhibited":
+            bc_gc, hc_gc = 0, 0
+
+        elif network_type != "full":
+            raise ValueError(
+                """network_type must be 'full',
+                'no-feedback', 'no-feedforward' or 'disinhibited'"""
+            )
+
         if (type(spatial_patterns_gcs) == np.ndarray and
            type(temporal_patterns) == np.ndarray):
             for pa in range(len(spatial_patterns_gcs)):
@@ -74,24 +104,24 @@ class TunedNetwork(gennetwork.GenNetwork):
                                                       temporal_patterns[pa],
                                                       spatial_patterns_bcs[pa],
                                                       'ddend', 6.3, 0, 1, 0, 0,
-                                                      pp_weight)
+                                                      pp_bc)
 
         # GC -> MC
         gennetwork.tmgsynConnection(self.populations[0], self.populations[1],
                                     12, 'proxd', 1, 7.6, 500, 0.1, 0, 0, 10,
-                                    1.5, 2e-2)
+                                    1.5, gc_mc)
 
         # GC -> BC
         # Weight x4, target_pool = 2
         gennetwork.tmgsynConnection(self.populations[0], self.populations[2],
                                     8, 'proxd', 1, 8.7, 500, 0.1, 0, 0, 10,
-                                    0.8, 2.5e-2)
+                                    0.8, gc_bc)
 
         # GC -> HC
         # Divergence x4; Weight doubled; Connected randomly.
         gennetwork.tmgsynConnection(self.populations[0], self.populations[3],
                                     24, 'proxd', 1, 8.7, 500, 0.1, 0, 0, 10,
-                                    1.5, 2.5e-2)
+                                    1.5, gc_hc)
 
         # MC -> MC
         gennetwork.tmgsynConnection(self.populations[1], self. populations[1],
@@ -112,7 +142,7 @@ class TunedNetwork(gennetwork.GenNetwork):
         # # synapses x3; Weight *1/4; tau from 5.5 to 20 (Hefft & Jonas, 2005)
         gennetwork.tmgsynConnection(self.populations[2], self.populations[0],
                                     560, 'soma', 400, 20, 0, 1, 0, -70, 10,
-                                    0.85, 1.2e-3)
+                                    0.85, bc_gc)
 
         # We reseed here to make sure that those connections are consistent
         # between this and net_global which has a global target pool for
@@ -134,7 +164,7 @@ class TunedNetwork(gennetwork.GenNetwork):
         # Weight x10; Nr synapses x4; tau from 6 to 20 (Hefft & Jonas, 2005)
         gennetwork.tmgsynConnection(self.populations[3], self.populations[0],
                                     2000, 'dd', 640, 20, 0, 1, 0, -70, 10,
-                                    3.8, 6e-3)
+                                    3.8, hc_gc)
 
         # HC -> MC
         gennetwork.tmgsynConnection(self.populations[3], self.populations[1],
